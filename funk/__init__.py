4a3fffd75668b8ac21370ded50dccf3aa2e72f2d
maxhaubold <num-lims@med.uni-greifswald.de>
2026-03-19 12:14:04 +0100

    some changes

--- funk/__init__.py
+++ funk/__init__.py
@@ -5,23 +5,40 @@ import json
 import pathlib
 def chunk(infile:str, outdir:str, encoding:str="utf-8") -> list:
     """
-     chunk chunks a fhir resource into multiple files, each holding one
-     primary sample and its children. it assumes that the children
-     immediately follow their respective primary. it returns a list of files written.
+     chunk chunks a fhir resource into multiple files and returns a list of
+     files written.
+     
+     specimen: each written file holds one primary sample and its
+     children. it assumes that the children immediately follow their
+     respective primary.
+     
+     observation: it puts max 5 together into one file.
     """
-    f = open(infile, "r", encoding=encoding)
-    jsonin = json.load(f)
-    f.close()
-    entries = jsonin["entry"]
-    chunks = []
-    currentchunk = []
-    for entry in entries:
-        resource = figs.resource(entry)
-        if figs.category(resource) == "MASTER":
-            if len(currentchunk) > 0:
-                chunks.append(currentchunk)
-            currentchunk = [entry]
-        else:
-            currentchunk.append(entry)
-    stem = pathlib.Path(infile).stem
-    return fhirbuild.writeout(chunks, outdir, outname=stem)
+    with f = open(infile, "r", encoding=encoding):
+        jsonin = json.load(f)
+        entries = jsonin["entry"]
+        chunks = []
+        currentchunk = []
+        obschunks = []
+        currentobs = []
+        for entry in entries:
+            resource = figs.resource(entry)
+            if figs.resource_type(resource) == "Observation":
+                if len(currentobs) == 5:
+                    obschunks.append(currentobs)
+                    currentobs = []
+                currentobs.append(entry)
+            elif figs.resource_type(resource) == "Specimen":
+                if figs.category(resource) == "MASTER":
+                    if len(currentchunk) > 0:
+                        chunks.append(currentchunk)
+                    currentchunk = [entry]
+                else:
+                    currentchunk.append(entry)
+        if len(currentchunk) > 0:
+            chunks.append(currentchunk)
+        if len(currentobs) > 0:
+            obschunks.append(currentobs)
+        stem = pathlib.Path(infile).stem
+        #print(len(obschunks))
+        return fhirbuild.writeout(chunks + obschunks, outdir, outname=stem)
--- funk/__main__.py
+++ funk/__main__.py
@@ -1,6 +1,7 @@
 # automatically generated, DON'T EDIT. please edit main.ct from where this file stems.
 import argparse
 import funk
+import os
 import sys
 def main():
     """
@@ -8,11 +9,22 @@ def main():
      primary sample and its children.
     """
     parser = argparse.ArgumentParser(description="fhir chunk")
-    parser.add_argument("file", help="the file to chunk")
+    parser.add_argument("input", help="the input file or dir to chunk")
     parser.add_argument("outdir", help="the output directory")
-    parser.add_argument("-e", help="encoding")
+    parser.add_argument("-e", help="input encoding")
     args = parser.parse_args()    
-    out = funk.chunk(args.file, args.outdir, encoding=args.e)
+    out = []
+    if os.path.isdir(args.input):
+        files = os.listdir(args.input)
+        for file in files:
+            _, ext = os.path.splitext(file)
+            if ext != ".json":
+                continue
+            path = os.path.join(args.input, file)
+            written = funk.chunk(path, args.outdir, encoding=args.e)
+            out.extend(written)
+    else:
+        out = funk.chunk(args.input, args.outdir, encoding=args.e)
     for o in out:
         print(o)
 sys.exit(main())
